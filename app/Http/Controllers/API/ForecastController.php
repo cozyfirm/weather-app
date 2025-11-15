9625f8c3013d32654250f194508e1e442c49f753
aladinkapic <aladin.kapic@stu.ibu.edu.ba>
2025-11-15 14:28:53 +0100

    Preview city: Info about city data

--- app/Http/Controllers/API/ForecastController.php
+++ app/Http/Controllers/API/ForecastController.php
@@ -3,14 +3,23 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use \App\Http\Controllers\PublicPart\ForecastController as PublicForecastController;
 use App\Models\Base\Cities;
+use App\Models\Base\Forecast\FiveDays;
+use App\Models\Base\Forecast\TwelveHours;
+use App\Traits\API\ForecastTrait;
+use App\Traits\Common\CommonTrait;
 use App\Traits\Common\LogTrait;
 use App\Traits\Http\ResponseTrait;
+use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use temperatureHelper;
+use WindHelper;
 
 class ForecastController extends Controller{
-    use ResponseTrait, LogTrait;
+    use ResponseTrait, LogTrait, CommonTrait, ForecastTrait;
 
     /**
      * Get popular cities with temperatures and icons
@@ -38,6 +47,49 @@ class ForecastController extends Controller{
     /** ------------------------------------------------------------------------------------------------------------- */
 
     /**
+     * Get five days forecast; Preview 5 days with basic info
+     * @param $city
+     * @return array
+     */
+    public function getFiveDaysForecast($city): array{
+        $fiveDaysForecast = [];
+
+        foreach ($city->fiveDaysRel as $day){
+            $fiveDaysForecast[] = [
+                'id' => $day->id,
+                'cityKey' => $day->city_key,
+                'date' => $day->date,
+                'dateAndMonth' => $day->dateAndMonth(),
+                'weekDay' => $day->weekDay(),
+                'icon' => env('APP_URL') . ('/files/images/weathericons/') . ( $day->dayRel->icon ?? '1' ) . (".png"),
+                'minTemp' => ((temperatureHelper::roundUp($day->min_temp ?? '0')) . ("°C")),
+                'maxTemp' => ((temperatureHelper::roundUp($day->max_temp ?? '0')) . ("°C")),
+                'winDirection' => $day->dayRel->wind_direction_l ?? '',
+                'windSpeed' => $day->dayRel->wind_speed ?? ''
+            ];
+        }
+
+        return $fiveDaysForecast;
+    }
+
+    /**
+     * Get wind direction; Used in ForecastController::previewCity(Request $request);
+     *
+     * @param $city
+     * @return array
+     */
+    public function getInitWindDirection($city): array{
+        return [
+            'angle' => $city->twelveHoursCurrentRel->wind_direction_deg ?? '0',
+            'direction' => (("Iz pravca ") . ($city->twelveHoursCurrentRel->wind_direction_l ?? 'I') . ( "(" . ($city->twelveHoursCurrentRel->wind_direction_deg ?? 'I') . "°)" )),
+            'windSpeed' =>  $city->twelveHoursCurrentRel->wind_speed ?? '0km/h',
+            'windGust' => $city->twelveHoursCurrentRel->wind_gust_speed ?? '0km/h',
+            'windSpeedInfo' => WindHelper::windSpeed($city->twelveHoursCurrentRel->wind_speed ?? '0'),
+            'windGustInfo' => WindHelper::windGustSpeed($city->twelveHoursCurrentRel->wind_gust_speed ?? '0'),
+        ];
+    }
+
+    /**
      * Preview city basic info by slug
      *
      * @param Request $request
@@ -45,14 +97,97 @@ class ForecastController extends Controller{
      */
     public function previewCity(Request $request): JsonResponse{
         try{
-            dd($request->all());
+            // Check for slug
+            if(!isset($request->slug)) return $this->apiResponse('3051', __('Nepoznat grad'));
+            // Find city by slug in database
+            $dbCity = Cities::where('slug', '=', $request->slug)->first();
+            // Check for valid slug
+            if(!$dbCity) return $this->apiResponse('3052', __('Nepoznat grad'));
+
+            /** Now, let's fetch fresh data from city */
+            $publicFC = new PublicForecastController();
+
+            $city = $publicFC->getDayInfo($dbCity->key);
+            if(!$city) $city = Cities::where('key', '=', $dbCity->key)->first();
+
+            /* Update number of loads */
+            $city->update(['loads' => ($city->loads + 1)]);
+
+            /* Log search history */
+            $publicFC->userSearchHistory($city->key, $city->slug);
+
+            $twelveHours = $city->twelveHoursRel;
+
+            try{
+                /* Get info about sunset and sunrise */
+                $today = FiveDays::where('city_key', '=', $city->key)->where('date', '=', date('Y-m-d'))->first();
+                $tomorrow = FiveDays::where('city_key', '=', $city->key)->where('date', '=', Carbon::now()->addDay()->format('Y-m-d'))->first();
+
+                $sunrise = null;
+                $sunset  = null;
+
+                $startOfInterval = Carbon::parse($twelveHours[0]->date_time ?? '');
+                $endOfInterval   = Carbon::parse($twelveHours[$twelveHours->count() -1 ]->date_time ?? '');
+
+                $todaySunrise = Carbon::parse($today->sunrise);
+                $todaySunset  = Carbon::parse($today->sunset);
+
+                $tomorrowSunrise = Carbon::parse($tomorrow->sunrise);
+                $tomorrowSunset  = Carbon::parse($tomorrow->sunset);
+
+                if($todaySunrise >= $startOfInterval and $todaySunrise <= $endOfInterval){
+                    $sunrise = $todaySunrise;
+                }else if($todaySunset >= $startOfInterval and $todaySunset <= $endOfInterval){
+                    $sunset = $todaySunset;
+                }else if($tomorrowSunrise >= $startOfInterval and $tomorrowSunrise <= $endOfInterval){
+                    $sunrise = $tomorrowSunrise;
+                }else if($tomorrowSunset >= $startOfInterval and $tomorrowSunset <= $endOfInterval){
+                    $sunset = $tomorrowSunset;
+                }
+
+                if(isset($sunrise)){
+                    $sunriseSample = new TwelveHours();
+                    $sunriseSample->date_time = $sunrise->format('Y-m-d H:i:s');
+                    $sunriseSample->icon = "sunrise";
+
+                    $twelveHours->push($sunriseSample);
+                    $twelveHours = $twelveHours->sortBy(function($model){ return $model->date_time; });
+                }
+                if(isset($sunset)){
+                    $sunsetSample = new TwelveHours();
+                    $sunsetSample->date_time = $sunset->format('Y-m-d H:i:s');
+                    $sunsetSample->icon = "sunset";
+
+                    $twelveHours->push($sunsetSample);
+                    $twelveHours = $twelveHours->sortBy(function($model){ return $model->date_time; });
+                }
+
+            }catch (\Exception $e){
+                $this->write('ForecastController::popularCities() - Sunset & Sunrise', "3053", "Greška prilikom detekcije izlaska i zalaska sunca");
+            }
 
             return $this->apiResponse('0000', __('Success'), [
-
+                'info' => [
+                    'cityName' => $city->name,
+                    'dateTime' => $this->getFullDateTime(Carbon::now()),
+                    'description' => $city->twelveHoursCurrentRel->phase ?? '',
+                    'icon' => env('APP_URL') . ('/files/images/weathericons/') . ( $city->twelveHoursCurrentRel->icon ?? '1' ) . (".png"),
+                    'temperature' => temperatureHelper::roundUp($city->twelveHoursCurrentRel->temperature ?? '0') . ("°C"),
+                    'realFeel' => temperatureHelper::roundUp($city->twelveHoursCurrentRel->temperature_rf ?? '0') . ("°C"),
+                    'windSpeed' => (($city->twelveHoursCurrentRel->wind_speed ?? '') . (" km/h")),
+                    'humidity' => (($city->twelveHoursCurrentRel->rel_humidity ?? '') . (" %")),
+                    'visibility' => (($city->twelveHoursCurrentRel->visibility ?? '') . (" km")),
+                    'devPoint' => (($city->twelveHoursCurrentRel->dev_point ?? '') . ("°C"))
+                ],
+                "fiveDaysForecast" => $this->getFiveDaysForecast($city),
+                "windDirection" => $this->getInitWindDirection($city),
+                "twelveHoursForecast" => $twelveHours,
+                // 'city' => $city,
+                // 'history' => $this->getUserHistory()
             ]);
         }catch (\Exception $e){
-            $this->write('ForecastController::popularCities()', $e->getCode(), $e->getMessage(), $request);
-            return $this->apiResponse('3000', __('Greška prilikom obrade zahtjeva. Molimo kotantktirajte administratora!'));
+            $this->write('ForecastController::previewCity()', $e->getCode(), $e->getMessage(), $request);
+            return $this->apiResponse('3050', __('Greška prilikom obrade zahtjeva. Molimo kotantktirajte administratora!'));
         }
     }
 }
